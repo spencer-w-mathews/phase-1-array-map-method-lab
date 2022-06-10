7bd0f21175fb444654210bc834dd60735d50fa7d
Spencer Mathews <smathews0906@gmail.com>
2022-06-10 15:57:14 -0500

    commit tree 40d7ea2b021bd3674df07363d7e75ecb918c080d

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = ()=>{
+  const map1 = tutorials.map(capitalize)
+  return map1
+};
+ 
+//const map1 = tutorials.map(capitalize);
+
+/*function capitalize(input) {  
+  let CapitalizeWords = input[0].toUpperCase();  
+  for (let i = 1; i <= input.length - 1; i++) {  
+      let currentCharacter,  
+          previousCharacter = input[i - 1];  
+      if (previousCharacter && previousCharacter == ' ') {  
+          currentCharacter = input[i].toUpperCase();  
+      } else {  
+          currentCharacter = input[i];  
+      }  
+      CapitalizeWords = CapitalizeWords + currentCharacter;  
+  }  
+  return CapitalizeWords;  
+}  */
+
+function capitalize(input) {  
+  let words = input.split(' ');  
+  let CapitalizedWords = [];  
+  words.forEach(element => {  
+      CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
+  });  
+  return CapitalizedWords.join(' ');  
+}  
